e1ac1f86f0f467762826ba6fbd8f7507756e3b19
Marketa <habranova.marketa@gmail.com>
2022-04-19 23:17:57 +0200

    Added form for creating new author

--- library_app/ils/views.py
+++ library_app/ils/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView, CreateView
 from . import models
+from django.urls import reverse_lazy
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -12,6 +13,12 @@ class AuthorListView(ListView):
 class AuthorDetailView(DetailView):
     template_name = 'author/author_detail.html'
     model = models.Author
+
+class AuthorCreateView(CreateView):
+    template_name = 'author/author_create.html'
+    model = models.Author
+    fields = ['first_name', 'last_name']
+    success_url = reverse_lazy('author_list')
 
 class BookListView(ListView):
     template_name = 'book/book_list.html'

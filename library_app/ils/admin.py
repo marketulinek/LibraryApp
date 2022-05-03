d9ae4dbcf22ea69cacbd661c33e8ed87c44aa905
Marketa <habranova.marketa@gmail.com>
2022-05-03 22:44:29 +0200

    Created and registered model BookReservation

--- library_app/ils/admin.py
+++ library_app/ils/admin.py
@@ -4,3 +4,4 @@ import ils.models as models
 admin.site.register(models.Author)
 admin.site.register(models.Book)
 admin.site.register(models.Publisher)
+admin.site.register(models.BookReservation)
--- library_app/ils/models.py
+++ library_app/ils/models.py
@@ -1,4 +1,6 @@
+from asyncio.windows_events import NULL
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
@@ -22,6 +24,33 @@ class Book(models.Model):
 
     def __str__(self):
         return f"[{self.id}] {self.name}  ({self.author.first_name} {self.author.last_name}) "
+
+class BookReservation(models.Model):
+    COMPLETED = 'Completed'
+    CANCELLED = 'Cancelled'
+    FORFEITED = 'Forfeited'
+    TERMINATION_CHOICES = (
+        (COMPLETED, 'Reservation completed'),
+        (CANCELLED, 'Reservation cancelled'),
+        (FORFEITED, 'Reservation forfeited')
+    )
+
+    reader = models.ForeignKey(User, on_delete=models.RESTRICT)
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    book_available_at = models.DateTimeField(null=True, blank=True)
+    termination_type = models.CharField(max_length=10, choices=TERMINATION_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+
+        termination_info = ''
+        if self.termination_type:
+            termination_info = " / " + self.termination_type.upper()
+
+        return f"[{self.created_at}] {self.book.name} (reader: {self.reader.username}){termination_info}"
+
+    class Meta:
+        get_latest_by = "created_at"
 
 # TODO: Tag
 # TODO: Rating

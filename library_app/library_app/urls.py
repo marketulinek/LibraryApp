00af1ca7be3c268252b508f27e0e9ebaae53080f
Baruli <barbora3ulicna@gmail.com>
2022-05-11 17:40:06 +0200

    added conditional login-logout option

--- library_app/library_app/urls.py
+++ library_app/library_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ils.urls'))
+    path('', include('ils.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

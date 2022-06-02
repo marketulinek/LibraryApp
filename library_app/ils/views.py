07a0e9fe415126a3aa68071051e72e6f666a0f60
Marketa <habranova.marketa@gmail.com>
2022-06-02 09:22:28 +0200

    New view for making a reservation by post request

--- library_app/ils/views.py
+++ library_app/ils/views.py
@@ -1,6 +1,9 @@
-from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView, CreateView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.contrib import messages
 from . import models
 
 class IndexView(TemplateView):
@@ -43,3 +46,26 @@ class LibraryRulesView(TemplateView):
 
 class FAQView(TemplateView):
     template_name = 'for_readers/faq.html'
+
+
+# ------------------------------
+#         ACTION  VIEWS
+# ------------------------------
+
+class MakeReservationView(View):
+
+    def post(self, request, *args, **kwargs):
+
+        try:
+            pk_book = self.kwargs['pk']
+            chosen_book = models.Book.objects.get(pk=pk_book)
+
+            # Todo: if book is available (not exist open reservation for a book) -> fill in 'available_at'
+
+            models.BookReservation(reader=request.user.reader, book=chosen_book).save()
+            messages.success(request, 'Reservation was successfully created.')
+
+        except models.Reader.DoesNotExist:
+            messages.error(request, 'Permission denied. You are not a reader.')
+
+        return HttpResponseRedirect(reverse_lazy('book_detail', kwargs={'pk': pk_book}))

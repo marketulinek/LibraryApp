2cfbc2f905c90ce8ab17b84efd7f2b8bbbbc4ba4
Marketa <habranova.marketa@gmail.com>
2022-06-11 21:12:17 +0200

    Added Action column to the table with button for lending a book

--- library_app/ils/tables.py
+++ library_app/ils/tables.py
@@ -3,6 +3,11 @@ from ils.models import BookReservation
 
 
 class OpenReservationTable(tables.Table):
+    book = tables.Column(verbose_name='Book (author)')
+    book_available_at = tables.DateTimeColumn(format ='D d M Y')
+
     class Meta:
         model = BookReservation
         fields = ('reader', 'book', 'book_available_at')
+    
+    actions = tables.TemplateColumn(template_name='book_reservation/open_reservation_list_action_column.html', orderable=False)

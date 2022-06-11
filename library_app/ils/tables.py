5e60e9919fb6f800609f1d4f013fae9d27f6b5f2
Marketa <habranova.marketa@gmail.com>
2022-06-11 21:16:27 +0200

    Added ID column to the table

--- library_app/ils/tables.py
+++ library_app/ils/tables.py
@@ -3,11 +3,12 @@ from ils.models import BookReservation
 
 
 class OpenReservationTable(tables.Table):
+    id = tables.Column(verbose_name='#')
     book = tables.Column(verbose_name='Book (author)')
-    book_available_at = tables.DateTimeColumn(format ='D d M Y')
+    book_available_at = tables.DateTimeColumn(format='D d M Y')
 
     class Meta:
         model = BookReservation
-        fields = ('reader', 'book', 'book_available_at')
+        fields = ('id', 'reader', 'book', 'book_available_at')
     
     actions = tables.TemplateColumn(template_name='book_reservation/open_reservation_list_action_column.html', orderable=False)

d96361f5681f8a7f937a99fa9bee8c1e652b286b
Marketa <habranova.marketa@gmail.com>
2022-06-12 09:39:23 +0200

    Set up test file for testing the application

--- library_app/ils/tests.py
+++ library_app/ils/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ILSViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('testerka', 'terka.testerka@library.com', 'I.love.b00ks')

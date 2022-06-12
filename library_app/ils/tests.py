dd427d7bc73c9dea49831fda16623bc576bc8bff
Baruli <barbora3ulicna@gmail.com>
2022-06-12 22:40:08 +0200

    added more tests for user registration

--- library_app/ils/tests.py
+++ library_app/ils/tests.py
@@ -13,3 +13,30 @@ class ILSViewTests(TestCase):
     def test_my_account_not_signed(self):
         response = self.client.get(reverse('my_account'), follow=True)
         self.assertRedirects(response, '/accounts/login/?next=/my_account')
+
+
+    # Test registration form
+    def test_get_registration_form(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_user_can_register(self):
+        self.new_user = {'username': 'nova_testerka',
+                         "email": 'nova.testerka@library.com',
+                         "password1": 'I.love.b00ks',
+                         "password2": 'I.love.b00ks',
+                         "first_name": 'terezka',
+                         "last_name": 'testerka'}
+
+        self.register_url = reverse('register')
+        response = self.client.post(self.register_url, self.new_user, format="text/html")
+        self.assertRedirects(response, '/accounts/login/')
+
+
+    # Test book form
+    def test_get_book_create(self):
+        self.client.login(username='testerka', password='I.love.b00ks')
+        response = self.client.get(reverse('book_form'))
+        self.assertEqual(response.status_code, 200)
+
